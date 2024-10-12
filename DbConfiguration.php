f54adbea7d9a0b840575e39293cb4b1264360f12
guohan <poethan@foxmail.com>
2024-10-12 18:29:19 +0800

    feat: 优化Alias注解逻辑

--- DbConfiguration.php
+++ DbConfiguration.php
@@ -7,6 +7,9 @@ class DbConfiguration extends EzObject implements EzBean {
     public $host;
     public $user;
     public $pwd = "";
+    /**
+     * @Alias("db_type")
+     */
     public $dbType;
     public $port = 3306;
 }
